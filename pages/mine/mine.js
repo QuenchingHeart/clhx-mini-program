0a40dd374487b9e31d280024eabf56a42b38947d
erestu <849467474@qq.com>
2020-04-21 19:39:52 +0800

    mine的头像大一点 nickname不用cu-tag
    mine的头像大一点 nickname不用cu-tag

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,56 +1,39 @@
 // pages/mine/mine.js
 const app = getApp();
 var base64 = require("../images/base64");
-Component({
+Page({
 
   /**
    * 页面的初始数据
    */
-  Properties: {
-    tabbar: {
-      type:Object,
-      value:{}
-    },
-  },
+
   data:{
     userInfo:{
 
     },
-    nickname:''
+    nickname:'',
+    tabbar:{}
   },
   /**
  * 生命周期函数--监听页面加载
  */
-  lifetimes: {
-    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
-    attached: function () {
+  onLoad:function(){
 
-    },
-    moved: function () { },
-    detached: function () { },
   },
 
-  // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
-  attached: function () { }, // 此处attached的声明会被lifetimes字段中的声明覆盖
-  ready: function () {
-   },
-
-  pageLifetimes: {
-    // 组件所在页面的生命周期函数
-    show: function () {
-      app.editTabbar(); 
-      this.setData({
-        icon: base64.icon20,
-        userInfo: app.globalData.userInfo,
-        nickname: app.globalData.nickname
-      });
-    }
+  // 组件所在页面的生命周期函数
+  onShow: function () {
+    app.editTabbar(); 
+    this.setData({
+      icon: base64.icon20,
+      userInfo: app.globalData.userInfo,
+      nickname: app.globalData.nickname
+    });
   },
-  methods:{
-    toMakeUserInfo:function(){
-      wx.navigateTo({
-        url: '/pages/userInfo/makeUserInfo/makeUserInfo',
-      })
-    }
+  toMakeUserInfo:function(){
+    wx.navigateTo({
+      url: '/pages/userInfo/makeUserInfo/makeUserInfo',
+    })
   }
+  
 })
